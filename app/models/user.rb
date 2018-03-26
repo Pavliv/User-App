93d69c29fbc55801c08da4c47cfaa7bf0d735c7e
Maryan <maryan.pavliv@gmail.com>
2018-03-26 11:37:44 +0300

    create user app

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  # validates :first_name, length: { minimum: 5 }
+  # validates :last_name, length: { minimum: 5 }
+  # validates :age, length: { maximum: 3 }
+  # validates :age, numericality: { only_integer: true }
+end
